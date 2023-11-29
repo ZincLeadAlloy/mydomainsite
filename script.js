4d56c97f1d81c419ac9052adfdebb219046fa4c4
ZincLeadAlloy <121895466+ZincLeadAlloy@users.noreply.github.com>
2023-11-29 18:54:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+// script.js
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function () {
+        // Remove the 'active' class from all links
+        document.querySelectorAll('nav a').forEach(link => {
+            link.classList.remove('active');
         });
+
+        // Add the 'active' class to the clicked link
+        this.classList.add('active');
     });
 });
